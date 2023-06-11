9052289bafcfd91737b5c27fc369bfe6780ea9fb
CaioOliveira793 <caio.vsoliveira23@gmail.com>
2023-06-11 18:17:18 -0300

    feat(back): add app exception handler/filter

--- backend/src/module/App.ts
+++ backend/src/module/App.ts
@@ -1,9 +1,17 @@
+import { APP_FILTER } from '@nestjs/core';
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { createGlobalConfigOptions } from '@/config/ConfigOptions';
 import { IamModule } from '@/module/iam/IamModule';
+import { HttpExceptionFilter } from '@/filter/HttpExceptionFilter';
 
 @Module({
 	imports: [ConfigModule.forRoot(createGlobalConfigOptions()), IamModule],
+	providers: [
+		{
+			provide: APP_FILTER,
+			useClass: HttpExceptionFilter,
+		},
+	],
 })
 export class AppModule {}
