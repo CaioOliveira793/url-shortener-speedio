d5273332044bbb65234415d344c54bf7ab582002
CaioOliveira793 <caio.vsoliveira23@gmail.com>
2023-06-11 18:13:38 -0300

    feat(back): enable fastify cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,17 +4,19 @@ import {
 	FastifyAdapter,
 	NestFastifyApplication,
 } from '@nestjs/platform-fastify';
+import cors from '@fastify/cors';
 import { EnvVariables } from '@/config/Environment';
 import { AppModule } from '@/module/App';
 import { ShutdownSignal } from '@nestjs/common';
 
+const BODY_LIMIT = 20 * 1024 * 1024;
+
 async function bootstrap() {
 	const app = await NestFactory.create<NestFastifyApplication>(
 		AppModule,
-		new FastifyAdapter({
-			logger: true,
-		})
+		new FastifyAdapter({ logger: true, bodyLimit: BODY_LIMIT })
 	);
+	app.register(cors, { origin: true });
 
 	const configService = app.get<ConfigService<EnvVariables>>(ConfigService);
 	const port = configService.get<number>('PORT') as number;
