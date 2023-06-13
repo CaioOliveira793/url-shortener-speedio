527f69fdb497ac01eb6d5dd0a31bc5b1cc68c7c8
CaioOliveira793 <caio.vsoliveira23@gmail.com>
2023-06-13 17:17:36 -0300

    feat(front): improve NotFount page styles

--- frontend/src/page/NotFound.vue
+++ frontend/src/page/NotFound.vue
@@ -1,7 +1,21 @@
 <template>
-	<main>
-		<h1>A página que você procura não foi encontrada</h1>
-		<!-- TODO: page not found image -->
-		<img src="" alt="page-not-found" />
+	<main :class="$style.main_content">
+		<h1 class="heading3">A página que você procura não foi encontrada</h1>
+		<p>
+			Verifique sua URL ou volte para a
+			<router-link to="/">página inicial</router-link>.
+		</p>
+		<!-- TODO: add page not found image -->
 	</main>
 </template>
+
+<style module>
+.main_content {
+	display: flex;
+	flex-flow: column nowrap;
+	justify-content: center;
+	align-items: center;
+	margin: calc(var(--spacing-unit) * 4);
+	gap: calc(var(--spacing-unit) * 2);
+}
+</style>
